ae4d41b90d682d211c28f683148def685411bf70
MikaelCrispim <mikaelcrispims@gmail.com>
2023-06-09 11:58:05 -0300

    feature:add news archives css:login.css && verificacao.css && registros.css

--- src/login.php
+++ src/login.php
@@ -4,16 +4,18 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/css/style.css">
+    <link rel="stylesheet" href="../css/login.css">
     <title>GET E POST</title>
 </head>
 <body>
-    <div class="form_login">
-        <form action="registros.html" method="post">
-            <input type="text" name="email_login" placeholder="Email" required>
-            <input type="password" name="password_login" placeholder="Senha" required>
-            <input type="submit" value="Entrar" class="btnLogin" name="btnLogin">
-        </form>
+    <div class="formLoginArea">
+        <div class="form_login">
+            <form action="registros.html" method="post">
+                <input type="text" name="email_login" placeholder="Email" required><br>
+                <input type="password" name="password_login" placeholder="Senha" required><br>
+                <input type="submit" value="Entrar" class="btnLogin" name="btnLogin">
+            </form>
+        </div>
     </div>
 </body>
 </html>
