d042a9f940e18e573fd1d245cb30fb9e89a02511
Miguel Araújo <126573212+Migg-Araujo@users.noreply.github.com>
2023-06-08 21:04:32 -0300

    features: edit addDados

--- php/addDados.php
+++ php/addDados.php
@@ -1,10 +1,10 @@
 <?php
 include "conexao.php";
 
-$valor = $_POST["Valor"];
-$formapagamento = $_POST[""];
-$datapagamento = $_POST[""];
-$recebidaenviada = $_POST[""];
+$valor = $_POST["value"];
+$formapagamento = $_POST["formaPagamento"];
+$datapagamento = $_POST["dateRegister"];
+$recebidaenviada = $_POST["sacar"];
 
 $sql = "INSERT INTO TRANSACAO (valor, formapagamento, datapagamento, recebidaenviada)
 VALUES ('$valor', '$formapagamento', '$datapagamento', '$recebidaenviada')";
