0f797fb607f59d5203ea4af0db1c3d4b9d6afe9d
Miguel Araújo <126573212+Migg-Araujo@users.noreply.github.com>
2023-06-08 21:43:02 -0300

    feature: edit consultDados.php

--- php/sql/consultDados.php
+++ php/sql/consultDados.php
@@ -1,3 +1,13 @@
 <?php
-switch($_POST[""]);
+switch($_POST["verificacoes"]){
+    case 1:
+        $dateInicio = $_GET['dateInicio'];
+    break;
+    case 2:
+        break;
+    case 3:
+        break;
+    case 4:
+        break;
+};
 ?>
