a81cb9c641540bb53df46818c6200aec3a9f5e71
MikaelCrispim <mikaelcrispims@gmail.com>
2023-06-10 21:43:19 -0300

    feature:edit verificacao.php and registros.php

--- php/verificacao.php
+++ php/verificacao.php
@@ -1,22 +1,26 @@
 <?php
-if(!isset($_SESSION)) {
+if (!isset($_SESSION)) {
     session_start();
 }
 
-if(!isset($_SESSION['user'])){
+if (!isset($_SESSION['user'])) {
     die("Faça o login para acessar a página. <p><a href =\"./../index.php\">Entrar</a></p>");
 }
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-<meta charset="UTF-8">
+    <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>POST e GET</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz"
+        crossorigin="anonymous"></script>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="../css/verificacao.css">
 </head>
@@ -24,24 +28,28 @@ if(!isset($_SESSION['user'])){
     <nav class="navbar navbar-expand-lg">
         <div class="container-fluid">
             <a class="navbar-brand" href="#">DS Bank</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav justify-content-end flex-grow-1 pe-0">
-                <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="./../src/registros.php">Registrar transações</a>
-                </li>
-                <li class="nav-item">
-                <a class="nav-link" href="./verificacao.php">Consultar transações</a>
-                </li>
-                <li class="nav-item">
-                <a class="nav-link" href="./../php/login/logout.php">Logout</a>
-                </li>
-            </ul>
+                <ul class="navbar-nav justify-content-end flex-grow-1 pe-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="./../src/registros.php">Registrar
+                            transações</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="./verificacao.php">Consultar transações</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="./../php/login/logout.php">Logout</a>
+                    </li>
+                </ul>
             </div>
         </div>
     </nav>
+    <div class="form_verificacao">
     <form action="" method="get">
         <label>Qual a forma de filtragem?</label><br>
         <select name="verificacoes">
@@ -51,27 +59,27 @@ if(!isset($_SESSION['user'])){
             <option value="4" name="receivedSend">Recebida/Enviada</option>
         </select><br>
         <?php
-if(isset($_GET['verificacoes'])){
-    switch ($_GET['verificacoes']) {
-        case 1:
-            echo "
+        if (isset($_GET['verificacoes'])) {
+            switch ($_GET['verificacoes']) {
+                case 1:
+                    echo "
             
             <label>Deseja verificar depósitos que foram realizados em qual período:</label>
-            <br><input type=date name=dateInicio><br>
+            <br><input type=date placeholder='Ano/Mes/Dia' name=dateInicio><br>
             <input type=date name=dateFim><br>
             
             ";
-            break;
-        case 2:
-            echo "
+                    break;
+                case 2:
+                    echo "
             
             <label>Deseja verificar depósitos que foram realizados até qual quantia?</label>
             <br><input type=number placeholder=Valor name=value_ver step=any min=0><br>
             
             ";
-            break;
-        case 3:
-            echo "
+                    break;
+                case 3:
+                    echo "
             
             <label>Deseja verificar depósitos que foram realizados com qual forma de pagamento?</label>
             <select name=formaPagamento_ver><br>
@@ -83,9 +91,9 @@ if(isset($_GET['verificacoes'])){
             
             
             ";
-            break;
-        case 4:
-            echo"
+                    break;
+                case 4:
+                    echo "
             
             <label>Deseja verificar depósitos que foram enviados ou recebidos?</label>
             <select name=receivedSend_ver><br>
@@ -95,21 +103,24 @@ if(isset($_GET['verificacoes'])){
             
             
             ";
-            break;
-    }
-}
-?>
+                    break;
+            }
+        }
+        ?>
         <input type="submit" value="Ok" name="btnFilter" class="btnFilter">
-        <button><a href="./verificacao.php">Voltar</a></button>
     </form>
+    </div>
     <?php
-    if(isset($_GET['dateInicio']) or isset($_GET['value_ver']) or isset($_GET['formaPagamento_ver']) or isset($_GET['receivedSend_ver'])){
+    if (isset($_GET['dateInicio']) or isset($_GET['value_ver']) or isset($_GET['formaPagamento_ver']) or isset($_GET['receivedSend_ver'])) {
         include './sql/consultDados.php';
     }
     ?>
 </body>
 <script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js" integrity="sha384-fbbOQedDUMZZ5KreZpsbe1LCZPVmfTnH7ois6mU1QK+m14rQ1l2bGBq41eYeM/fS" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js"
+    integrity="sha384-fbbOQedDUMZZ5KreZpsbe1LCZPVmfTnH7ois6mU1QK+m14rQ1l2bGBq41eYeM/fS"
+    crossorigin="anonymous"></script>
 </script>
+
 </html>
